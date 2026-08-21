290ebc6736c31cd5eddf09230c9a5e6f5882a227
AmanGoel1990 <amangoel.goel90@gmail.com>
2026-08-21 15:59:26 +0530

    add routerlink in menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
